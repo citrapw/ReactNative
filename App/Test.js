b238e913c53f22fbd3620753df72854ddad6f930
Citrapw <citrapw@Citras-MacBook-Pro.local>
2016-08-03 11:36:15 +0700

    test

---
+++ App/Test.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet } from 'react-native';
+
+
+testing = StyleSheet.create({
+
+  mainContainer: {
+    backgroundColor: 'skyblue'
+  },
+  global: {
+    color: 'mistyrose',
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'blue',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    textAlign: 'center',
+    marginTop: 30,
+    height: 100
+  }
+
+})
+
+module.export = testing
